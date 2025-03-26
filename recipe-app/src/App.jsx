340f78b206371229001b30fa2b4e090c009c7aca
Gaurav <gauravraj7011@gmail.com>
2025-03-26 10:46:50 +0530

    components, and api fetching from edamam api

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,25 +1,57 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import Recipe from "./components/Recipe";
 
 function App() {
+  // const APP_ID = "618b2754";
+  // const APP_KEY = "fe545478b74e8e4fcc32a68dd3f5aa80";
+  const APP_ID = "705db6fc";
+  const APP_KEY = "adf25552569a08da03a991eaa3a287ed";
+
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Indian Chai");
+  const [dishes, setDishes] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      let res = await fetch(
+        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`,
+        {
+          headers: {
+            "Edamam-Account-User": "GauravRecipe",
+          },
+        }
+      );
+      let response = await res.json();
+      setDishes(response.hits);
+    }
+    getData();
+  }, [query]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <>
-      <div>
-        <nav className="navbar bg-body-tertiary">
-          <div className="container-fluid">
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </nav>
-      </div>
+      <form className="d-flex" onSubmit={submitHandler}>
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button className="btn btn-dark" type="submit">
+          Search
+        </button>
+      </form>
+      {dishes.map((item, idx) => (
+        <Recipe recipeList={item} key={idx} />
+      ))}
     </>
   );
 }
